dbbdee8b96fbb3f9d9aa687f643f439d0ab7f754
jafflerbach <afflerbach@nceas.ucsb.edu>
2015-05-19 16:56:36 -0700

    added google vis functionality with acid dataset

--- server.r
+++ server.r
@@ -1,38 +1,36 @@
 #Server
 
-library(dplyr)
 
-shinyServer(function(input, output, session) {
+
+library(dplyr)
+library(googleVis)
+library(shiny)
+
+
+acid <- read.csv('acid.csv')%>%
+        filter(sp_type=='eez')
+
+shinyServer(function(input, output) {
   
-  # Provide explicit colors for regions, so they don't get recoded when the
-  # different series happen to be ordered differently from year to year.
-  # http://andrewgelman.com/2014/09/11/mysterious-shiny-things/
-  defaultColors <- c("#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477","#12dc9e","#dc1250","#e9c0fa")
-  series <- structure(
-    lapply(defaultColors, function(color) { list(color=color) }),
-    names = levels(data$wrld_rgn)
-  )
-  
-  yearData <- reactive({
-    # Filter to the desired year, and put the columns
-    # in the order that Google's Bubble Chart expects
-    # them (name, x, y, color, size). Also sort by region
-    # so that Google Charts orders and colors the regions
-    # consistently.
-    df <- data %>%
-      filter(Year == input$year) %>%
-      select(rgn_name,pressure_score) %>%
-      arrange(wrld_rgn)
+  dataInput <-reactive({
+    switch(input$dataset,
+                   'acid'=acid)
+    
   })
   
-  output$chart <- reactive({
-    # Return the data and options
-    list(
-      data = googleDataTable(yearData()),
-      options = list(
-        title = "Ocean Acidification over Time",
-        series = series
-      )
-    )
+  # output$histPlot <- renderPlot({
+  
+  #   histogram(ref())
+  # })
+  
+  output$motionChart<- renderGvis({
+    
+    gvisMotionChart(dataInput(),idvar="rgn_name",timevar="year")
+
+    
   })
-})
+  
+
+      
+    
+  })
