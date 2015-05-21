1b7f813651b06b41c1f5aa8e2d2e6de490c959c2
jafflerbach <afflerbach@nceas.ucsb.edu>
2015-05-21 10:20:17 -0700

    comment out later steps

--- server.r
+++ server.r
@@ -1,40 +1,49 @@
-#Server
+#Server.R
 
 
-library(dplyr)
-library(googleVis)
-library(shiny)
-library(ggplot2)
+#----------------------------------------
+# SETUP
+
+
+#libraries
+
+  library(dplyr)
+  library(googleVis)
+  library(shiny)
+  library(ggplot2)
+
 
 #dataset
 
+
+# FAO fishing dataset, aggregated catch by country and year
 
 fao <- read.csv('fao.csv',stringsAsFactors=F)%>%
         group_by(Country,Year)%>%
         summarise(Catch=sum(Catch))
 
-countries <- unique(fao$Country)
+#countries = unique(fao$Country)
 
+#----------------------------------------
+
+#SHINY SERVER FUNCTION
 
 shinyServer(function(input, output) {
 
-  
-  
   year <- reactive({input$year})
   
-  country <- reactive({input$country})
+  sliderValue <- reactive({input$n})
   
-  sliderValue <- reactive({
-    input$n
-  })
+#  country <- reactive({input$country})
 
 #create barplot
 
 barData <- reactive({
+  
   d = as.data.frame(fao)%>%
-  filter(Year==year())%>%
-  arrange(desc(Catch))%>%
-  slice(1:sliderValue())
+       filter(Year==year())%>%
+        arrange(desc(Catch))%>%
+         slice(1:sliderValue())
 
 })
     
@@ -45,29 +54,30 @@ barData <- reactive({
   
    })
 
-lineData <- reactive({
-  d = as.data.frame(fao)%>%
-      arrange(Year)%>%
-      filter(Country==country())
-})
+#  lineData <- reactive({
+            
+#    d = as.data.frame(fao)%>%
+#          arrange(Year)%>%
+#           filter(Country==country())
+#})
 
-output$lineChart <- renderPlot({
+#output$lineChart <- renderPlot({
   
-  ggplot(data=lineData(),aes(x=Year,y=Catch))+geom_line(size=1.5)
+#  ggplot(data=lineData(),aes(x=Year,y=Catch))+geom_line(size=1.5)
   
-})
+#})
   
 #Create GoogleVis plot using google vis package
 
-  output$motionChart<- renderGvis({
+#  output$motionChart<- renderGvis({
     
     
-    gData = fao%>%
-              group_by(Country,Year)%>%
-                summarise(Catch = sum(Catch))
+#    gData = fao%>%
+#              group_by(Country,Year)%>%
+#                summarise(Catch = sum(Catch))
     
-    gvisMotionChart(gData,idvar="Country",timevar="Year")
+#    gvisMotionChart(gData,idvar="Country",timevar="Year")
 
 
-  })
+#  })
 })
--- ui.r
+++ ui.r
@@ -2,26 +2,30 @@
 
 library(shiny)
 
-
+# Define UI for app
 shinyUI(fluidPage(
   
+  #App title
   titlePanel("Shiny Demo: FAO Fishing Data"),
 
+  # Sidebar with multiple toggles for input choices  
   sidebarLayout(
     sidebarPanel(
       selectInput('year','Year',choices = c(1950:2012)),
-      sliderInput("n", "Top # Countries:", min = 1, max = 100, value = 20),
-      selectInput('country','Country',choices = countries)),
+      sliderInput("n", "Top # Countries:", min = 1, max = 100, value = 20)#,
+#      selectInput('country','Country',choices = countries)
+    ),
   
+# The main panel where we want out plots to output
   mainPanel(
     tabsetPanel(
-      tabPanel("Bar Plot",plotOutput("barPlot")),
-      tabPanel("Line Chart",plotOutput("lineChart")),
-      tabPanel("GoogleVis",h4("FAO Fishing Data"),htmlOutput("motionChart"))
-
+      tabPanel("Bar Plot",plotOutput("barPlot"))#,
+#      tabPanel("Line Chart",plotOutput("lineChart")),
+#      tabPanel("GoogleVis",h4("FAO Fishing Data"),htmlOutput("motionChart"))
       )
     ) 
+  )
+ )
 )
-)
-)
+
 
